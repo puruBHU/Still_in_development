dfd325a1cdfeb44e770566f56c05062a5b10a951
puruBHU <purnendu06@gmail.com>
2019-07-26 20:14:13 +0530

    model created

--- ssd300_model.py
+++ ssd300_model.py
@@ -6,8 +6,124 @@ Created on Fri Jul 26 17:12:43 2019
 @author: Purnendu Mishra
 """
 from keras.models import Model
-from keras.layers import Input
+from keras.layers import Input, MaxPooling2D, ZeroPadding2D, Reshape
 from utility import conv_bn_relu
+from keras import backend as K
 
-def SSD300():
-    pass
+def SSD300(input_shape = (None, None, 3), anchors = [4, 6,6,6,4,4], num_classes = 21):
+    input_ = Input(shape = input_shape)
+    
+    # Block 01
+    x =  conv_bn_relu(filters = 64, 
+                      name = {'conv':'conv1_1', 'batch_norm':'bn1_1','activation':'relu_1_1'})(input_)
+    x =  conv_bn_relu(filters = 64, 
+                      name = {'conv':'conv1_2', 'batch_norm':'bn1_2','activation':'relu_1_2'})(x)
+    x =  MaxPooling2D(pool_size=(2,2), name = 'pool1')(x)
+    
+    # Block 02
+    x = conv_bn_relu(filters = 128, 
+                     name = {'conv':'conv2_1', 'batch_norm':'bn2_1','activation':'relu_2_1'})(x)
+   
+    x = conv_bn_relu(filters = 128, 
+                     name = {'conv':'conv2_2', 'batch_norm':'bn2_2','activation':'relu_2_2'})(x)
+     
+    x = MaxPooling2D(pool_size = (2,2), name='pool2')(x)
+    
+    # Block 3
+    x = conv_bn_relu(filters = 256, 
+                     name = {'conv':'conv3_1', 'batch_norm':'bn3_1','activation':'relu_3_1'})(x)
+    
+    x = conv_bn_relu(filters = 256, 
+                     name = {'conv':'conv3_2', 'batch_norm':'bn3_2','activation':'relu_3_2'})(x)
+    
+    x = conv_bn_relu(filters = 256, 
+                     name = {'conv':'conv3_3', 'batch_norm':'bn3_3','activation':'relu_3_3'})(x)
+    
+    # Apdding zeros to get layer shape as mentioned in the paper
+    x = ZeroPadding2D(padding = (1,1), name = 'zero_01')(x)
+    
+    x = MaxPooling2D(pool_size = (2,2), name='pool3')(x)
+    
+    # Block 4
+    x = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv4_1', 'batch_norm':'bn4_1','activation':'relu_4_1'})(x)
+    
+    x = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv4_2', 'batch_norm':'bn4_2','activation':'relu_4_2'})(x)
+    
+    conv4_3_out = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv4_3', 'batch_norm':'bn4_3','activation':'relu_4_3'})(x)
+    
+    x = MaxPooling2D(pool_size = (2,2), name='pool4')(conv4_3_out)
+    
+    # Block 5
+    x = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv5_1', 'batch_norm':'bn5_1','activation':'relu_5_1'})(x)
+    
+    x = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv5_2', 'batch_norm':'bn5_2','activation':'relu_5_2'})(x)
+    
+    x = conv_bn_relu(filters = 512, 
+                     name = {'conv':'conv5_3', 'batch_norm':'bn5_3','activation':'relu_5_3'})(x)
+    
+    x = MaxPooling2D(pool_size = (1,1), name = 'pool5')(x)
+    
+    #Auxilary Layer
+    fc6 = conv_bn_relu(filters = 1024, 
+                      name = {'conv':'conv_fc6', 'batch_norm':'bn_fc6','activation':'relu_fc6'})(x)
+    
+    fc7 = conv_bn_relu(filters = 1024, kernel_size = (1,1), stride= (2,2),
+                      name = {'conv':'conv_fc7', 'batch_norm':'bn_fc7','activation':'relu_fc7'})(fc6)
+    
+    conv8_1 = conv_bn_relu(filters = 256, kernel_size = (1,1),
+                      name = {'conv':'conv8_1', 'batch_norm':'bn8_1','activation':'relu_8_1'})(fc7)
+    
+    conv8_2 = conv_bn_relu(filters = 512, kernel_size = (3,3), strides = (2,2),
+                      name = {'conv':'conv8_2', 'batch_norm':'bn8_2','activation':'relu_8_2'})(conv8_1)
+    
+    # Block 7
+    conv9_1 = conv_bn_relu(filters = 128, kernel_size = (1,1),
+                      name = {'conv':'conv9_1', 'batch_norm':'bn9_1','activation':'relu_9_1'})(conv8_2)
+    
+    conv9_2 = conv_bn_relu(filters = 256, kernel_size = (3,3), strides = (2,2),
+                      name = {'conv':'conv9_2', 'batch_norm':'bn9_2','activation':'relu_9_2'})(conv9_1)
+    
+    # Block 8
+    conv10_1 = conv_bn_relu(filters = 128, kernel_size = (1,1),
+                      name = {'conv':'conv10_1', 'batch_norm':'bn10_1','activation':'relu_10_1'})(conv9_2)
+    
+    
+    
+    conv10_2 = conv_bn_relu(filters = 256, kernel_size = (3,3), strides = (2,2), padding='same',
+                      name = {'conv':'conv10_2', 'batch_norm':'bn10_2','activation':'relu_10_2'})(conv10_1)
+    
+    
+    
+    # Block 9
+    conv11_1 = conv_bn_relu(filters = 128, 
+                      name = {'conv':'conv11_1', 'batch_norm':'bn11_1','activation':'relu_11_1'})(conv10_2)
+    
+    conv11_2 = conv_bn_relu(filters = 256, kernel_size = (3,3), strides = (1,1),  padding='valid',
+                      name = {'conv':'conv11_2', 'batch_norm':'b11_2','activation':'relu_11_2'})(conv11_1)
+    
+    
+    
+    # Calculate the spatial dimension of output layer
+    conv4_3_dim = K.int_shape(conv4_3_out)[1:-1]
+    fc7_dim     = K.int_shape(fc7)[1:-1]
+    conv8_2_dim = K.int_shape(conv8_2)[1:-1]
+    conv9_2_dim = K.int_shape(conv9_2)[1:-1]
+    conv10_2_dim  = K.int_shape(conv10_2)[1:-1]
+    conv11_2_dim  = K.int_shape(conv11_2)[1:-1]
+    
+    
+    
+    
+    return Model(inputs = input_, outputs = conv11_2)
+
+
+
+if __name__ == '__main__':
+    model = SSD300(input_shape = (300, 300, 3))
+    model.load_weights('VGG_ILSVRC_16_layers_fc_reduced.h5', by_name = True)
+#    model.summary()
--- utility.py
+++ utility.py
@@ -16,19 +16,14 @@ def _bn_relu(input_):
 
 def conv_bn_relu(**params):
     filters     = params['filters']
-    kernel_size = params['kernel_size']
+    kernel_size = params.setdefault('kernel_size', (3,3))
     strides     = params.setdefault('strides',(1,1))
     padding     = params.setdefault('padding','same')
     dilation_rate = params.setdefault('dilation_rate', 1)
     kernel_initializer = params.setdefault('kernel_initializer', he_normal())
     kernel_regularizer = params.setdefault('kernel_regularizer', l2(1e-3))
     activation         = params.setdefault('activation','relu')
-    name               = params.setdefault('name',None)
-
-    if not name == None:
-        conv_name = 'conv_{}'.format(name)
-        bn_name   = 'BN_{}'.format(name) 
-        act_name  = 'Act_{}_{}'.format(name, activation)
+    name               = params.setdefault('name', None)
 
     def f(input_):
         conv = Conv2D(filters       = filters,
@@ -38,47 +33,12 @@ def conv_bn_relu(**params):
                       dilation_rate = dilation_rate,
                       kernel_initializer = kernel_initializer,
                       kernel_regularizer = kernel_regularizer,
-                      name=conv_name)(input_)
+                      name = name['conv'])(input_)
 
-        batch_norm = BatchNormalization(name=bn_name)(conv)
+        batch_norm = BatchNormalization(name = name['batch_norm'])(conv)
 
-        return Activation(activation,name=act_name)(batch_norm)
+        return Activation(activation,name = name['activation'])(batch_norm)
     return f
 
 
-def sep_conv_bn_relu(**params):
-    filters     = params['filters']
-    kernel_size = params.setdefault('kernel_size',(3,3))
-    strides     = params.setdefault('strides',(1,1))
-    padding     = params.setdefault('padding','same')
-    dilation_rate = params.setdefault('dilation_rate', 1)
-    depthwise_initializer = params.setdefault('depthwise_initializer', he_normal())
-    pointwise_initializer = params.setdefault('pointwise_initializer', he_normal())
 
-    depthwise_regularizer = params.setdefault('depthwise_regularizer', l2(1e-3))
-    pointwise_regularizer = params.setdefault('pointwise_regularizer', l2(1e-3))
-    activation         = params.setdefault('activation','relu')
-    name               = params.setdefault('name',None)
-
-    if not name == None:
-        conv_name = 'conv_{}'.format(name)
-        bn_name   = 'BN_{}'.format(name) 
-        act_name  = 'Act_{}_{}'.format(name, activation)
-
-    def f(input_):
-        conv = SeparableConv2D(filters       = filters,
-                              kernel_size   = kernel_size,
-                              strides       = strides,
-                              padding       = padding,
-                              dilation_rate = dilation_rate,
-                              depthwise_initializer=depthwise_initializer,
-                              pointwise_initializer=pointwise_initializer,
-                              depthwise_regularizer=depthwise_regularizer,
-                              pointwise_regularizer=pointwise_regularizer,
-                              name=conv_name
-                              )(input_)
-
-        batch_norm = BatchNormalization(name=bn_name)(conv)
-
-        return Activation(activation, name=act_name)(batch_norm)
-    return f
